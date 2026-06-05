cd6978153f82575c977ccac632b27f14263f6794
Tdrama <tbamidele021@gmail.com>
2026-06-05 19:59:29 +0100

    fix requirements for render deployment

--- run.py
+++ run.py
@@ -1,4 +1,12 @@
+import os
 import uvicorn
 
+PORT = int(os.getenv("PORT", 8000))
+
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(
+        "app.main:app",
+        host="0.0.0.0",
+        port=PORT,
+        reload=False
+    )
